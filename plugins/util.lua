4744c172c5da23de554396b8df4aace145c635ec
paradox8599 <paradox8599@outlook.com>
2023-12-24 05:44:47 +1100

    plugins added

--- plugins/util.lua
+++ plugins/util.lua
@@ -1,16 +1,29 @@
 return {
-  "glacambre/firenvim",
-  lazy = not vim.g.started_by_firenvim,
-  build = function() vim.fn["firenvim#install"](0) end,
-  config = function()
-    vim.g.firenvim_config = {
-      globalSettings = { alt = "all" },
-      localSettings = {
-        [".*"] = {
-          content = "text",
-          takeover = "never",
+  {
+    "glacambre/firenvim",
+    lazy = not vim.g.started_by_firenvim,
+    build = function() vim.fn["firenvim#install"](0) end,
+    config = function()
+      vim.g.firenvim_config = {
+        globalSettings = { alt = "all" },
+        localSettings = {
+          [".*"] = {
+            content = "text",
+            takeover = "never",
+          },
         },
-      },
-    }
-  end,
+      }
+    end,
+  },
+  {
+    -- Detect tabstop and shiftwidth automatically
+    "tpope/vim-sleuth",
+    event = "VeryLazy",
+  },
+  {
+    -- <C-a> for date
+    "tpope/vim-speeddating",
+    event = "VeryLazy",
+    config = function() end,
+  },
 }
