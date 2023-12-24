01f5f697fbcc2c0a6624f6efbc20cf1c1a826cca
paradox8599 <paradox8599@outlook.com>
2023-12-24 15:41:07 +1100

    plugin switch from telescope undo ext to undotree

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,7 +8,7 @@ return {
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 
   { import = "astrocommunity/editing-support/todo-comments-nvim" },
-  { import = "astrocommunity/editing-support/telescope-undo-nvim" },
+  -- { import = "astrocommunity/editing-support/telescope-undo-nvim" },
 
   { import = "astrocommunity/terminal-integration/vim-tpipeline" },
   { import = "astrocommunity/terminal-integration/flatten-nvim" },
--- plugins/util.lua
+++ plugins/util.lua
@@ -74,4 +74,18 @@ return {
       },
     },
   },
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader><leader>u", vim.cmd.UndotreeToggle, desc = "[U]ndo tree toggle" },
+    },
+    config = function()
+      if jit.os == "Windows" then
+        vim.opt.undodir = os.getenv "UserProfile" .. "/.vim/undodir"
+      else
+        vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+      end
+      vim.opt.undofile = true
+    end,
+  },
 }
