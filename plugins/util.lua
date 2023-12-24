7df4f1fba8aa4a911f21e9962927d7c08d9234e1
paradox8599 <paradox8599@outlook.com>
2023-12-24 15:45:39 +1100

    extracted harppon into separate file as too long

--- plugins/util.lua
+++ plugins/util.lua
@@ -16,68 +16,22 @@ return {
     end,
   },
   {
-    -- Detect tabstop and shiftwidth automatically
-    "tpope/vim-sleuth",
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
     event = "VeryLazy",
   },
   {
-    -- <C-a> for date
-    "tpope/vim-speeddating",
+    "tpope/vim-speeddating", -- <C-a/x> for date
     event = "VeryLazy",
     config = function() end,
   },
   {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    requires = { { "nvim-lua/plenary.nvim" } },
-    opts = {},
-    keys = {
-      {
-        "<leader><leader>a",
-        function() require("harpoon.mark").add_file() end,
-        desc = "Add file to marks",
-      },
-      {
-        "<leader><leader>d",
-        function() require("harpoon.mark").rm_file() end,
-        desc = "Remove file from marks",
-      },
-      {
-        "<leader><leader>c",
-        function() require("harpoon.mark").clear_all() end,
-        desc = "Clear marks",
-      },
-      {
-        "<leader>m",
-        function() require("harpoon.ui"):toggle_quick_menu() end,
-        desc = "Toggle marks quick menu",
-      },
-      {
-        "<C-m>",
-        function()
-          vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
-            local num = tonumber(input)
-            if num then require("harpoon.ui").nav_file(num) end
-          end)
-        end,
-        desc = "Goto index of mark",
-      },
-      {
-        "<C-p>",
-        function() require("harpoon.ui").nav_prev() end,
-        desc = "Goto previous mark",
-      },
-      {
-        "<C-n>",
-        function() require("harpoon.ui").nav_next() end,
-        desc = "Goto next mark",
-      },
-    },
-  },
-  {
     "mbbill/undotree",
     keys = {
-      { "<leader><leader>u", vim.cmd.UndotreeToggle, desc = "[U]ndo tree toggle" },
+      {
+        "<leader><leader>u",
+        vim.cmd.UndotreeToggle,
+        desc = "[U]ndo tree toggle",
+      },
     },
     config = function()
       if jit.os == "Windows" then
